b30bfe199074087ddd4d7e6231495299234e05f2
Dolgih Natalia <dolgihnatasha@users.noreply.github.com>
2015-10-29 18:40:52 +0500

    Update emitter.js

--- emitter.js
+++ emitter.js
@@ -1,23 +1,35 @@
+'use strict';
+
 module.exports = function () {
     return {
+        students: [],
         on: function (eventName, student, callback) {
-
+            if (this.students.indexOf(student) === -1) {
+                this.students.push(student);
+            }
+            if (!student.hasOwnProperty('events')){
+                student.events = {};
+            }
+            student.events[eventName] = callback;
         },
-
         off: function (eventName, student) {
-
+            var studentIndex = this.students.indexOf(student);
+            if (studentIndex !== -1) {
+                if(this.students[studentIndex].events.hasOwnProperty(eventName)) {
+                    delete this.students[studentIndex].events[eventName];
+                }
+            }
         },
-
         emit: function (eventName) {
-
-        },
-
-        several: function (eventName, student, callback, n) {
-
-        },
-
-        through: function (eventName, student, callback, n) {
-
+            var events = eventName.split('.');
+            for (var i = 0; i < this.students.length; i++) {
+                for (var j = 0; j < events.length; j++) {
+                    var emittedEvent = events.slice(0, j + 1).join('.');
+                    if (this.students[i].events.hasOwnProperty(emittedEvent)) {
+                        this.students[i].events[emittedEvent].apply(this.students[i]);
+                    }
+                }
+            }
         }
     };
 };

f700fa62d9622ab95d1620f950848f19fdbf9342
slavavikharev <slavavicharev@gmail.com>
2015-10-30 23:49:31 +0500

    First try

--- emitter.js
+++ emitter.js
@@ -1,23 +1,43 @@
 module.exports = function () {
-    return {
-        on: function (eventName, student, callback) {
+    return new Emmiter();
+};
 
-        },
+function Emmiter() {
+    this.students = new Set();
+}
 
-        off: function (eventName, student) {
+Emmiter.prototype.on = function (eventName, student, callback) {
+    if (!student.events) {
+        student.events = {};
+    }
+    student.events[eventName] = callback;
+    this.students.add(student);
+};
 
-        },
-
-        emit: function (eventName) {
-
-        },
-
-        several: function (eventName, student, callback, n) {
-
-        },
-
-        through: function (eventName, student, callback, n) {
-
+Emmiter.prototype.off = function (eventName, student) {
+    for (event in student.events) {
+        if (event === eventName || event.split('.')[0] === eventName) {
+            delete student.events[event];
         }
-    };
+    }
+};
+
+Emmiter.prototype.emit = function (eventName) {
+    this.students.forEach(student => {
+        if (student.events[eventName]) {
+            student.events[eventName].call(student);
+        }
+        var root = eventName.split('.')[0];
+        if (root !== eventName && student.events[root]) {
+            student.events[root].call(student);
+        }
+    });
+};
+
+Emmiter.prototype.several = function (eventName, student, callback, n) {
+
+};
+
+Emmiter.prototype.through = function (eventName, student, callback, n) {
+
 };

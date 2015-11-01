a80535001d039ffcc7059c7e47b535cd66745d72
air-breathing <grin.nadezh@yandex.ru>
2015-11-01 22:01:09 +0500

    Update emitter.js

--- emitter.js
+++ emitter.js
@@ -1,19 +1,63 @@
 module.exports = function () {
     return {
+        students: [],
         on: function (eventName, student, callback) {
+            var eventLayers = eventName.split('.');
+            var studentWithEvent = student;
+            var isNewStudent = this.students.indexOf(student);
+            var currentLayer = studentWithEvent;
+            for (var i in eventLayers){
+                if (currentLayer[eventLayers[i]] === undefined)
+                {
+                    currentLayer[eventLayers[i]] = {}
 
+                }
+                currentLayer = currentLayer[eventLayers[i]];
+            }
+            currentLayer['action'] = callback;
+            if (isNewStudent == -1) {
+                this.students.push(studentWithEvent);
+            }
         },
 
         off: function (eventName, student) {
-
+            var indexCurrentStudents = this.students.indexOf(student);
+            var eventLayers = eventName.split('.');
+            var currentLayer = this.students[indexCurrentStudents];
+            for (var i = 0; i < eventLayers.length - 1; i++){
+                currentLayer = currentLayer[eventLayers[i]];
+            }
+            delete currentLayer[eventLayers[eventLayers.length - 1]];
+            console.log(this.students);
         },
 
         emit: function (eventName) {
+            //console.log(eventName, 'event');
+            var eventLayers = eventName.split('.');
+            for (var index in this.students){
+                var currentLayer = this.students[index];
+                var stackOfFunctions = [];
+                for (var i = 0; i < eventLayers.length; i++)
+                {
+                    if(currentLayer[eventLayers[i]] === undefined)
+                    {
+                        break;
+                    }
+                    stackOfFunctions.push(currentLayer[eventLayers[i]]);
+                    currentLayer = currentLayer[eventLayers[i]];
+                }
+                //console.log(stackOfFunctions);
+                for (var i in stackOfFunctions)
+                {
+                    var action = stackOfFunctions.pop().action;
+                    var bindedAction = action.bind(this.students[index]);
+                    bindedAction();
+                }
+            }
 
         },
 
         several: function (eventName, student, callback, n) {
-
         },
 
         through: function (eventName, student, callback, n) {

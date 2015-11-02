4fb93dbb9a474ed3f3dad63cf9cfd3527a477e12
Anna1004 <anna.khlutkova@mail.ru>
2015-11-03 00:08:44 +0500

    Постараюсь доработать быстрее.

--- emitter.js
+++ emitter.js
@@ -1,15 +1,34 @@
 module.exports = function () {
+    var students = [];
     return {
         on: function (eventName, student, callback) {
-
+            var newStudent = {
+                dataOfStudent: student,
+                event: eventName,
+                callBack: callback
+            };
+            students.push(newStudent);
         },
 
         off: function (eventName, student) {
-
+            var index;
+            for (var i = 0; i < students.length; i++) {
+                if (students[i] !== undefined &&
+                    students[i].event === eventName &&
+                    students[i].dataOfStudent === student) {
+                    index = i;
+                }
+            }
+            delete students[index];
         },
 
         emit: function (eventName) {
-
+            for (var j = 0; j < students.length; j++) {
+                if (students[j] !== undefined &&
+                    students[j].event === eventName) {
+                    students[j].callBack.call(students[j].dataOfStudent);
+                }
+            }
         },
 
         several: function (eventName, student, callback, n) {

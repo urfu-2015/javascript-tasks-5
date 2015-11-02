ae9d53c8fed6fe7831c425fd8c59fc26902dcf78
Rybalka Ekaterina <lonelygopnik@gmail.com>
2015-11-02 23:15:29 +0500

    Update emitter.js

--- emitter.js
+++ emitter.js
@@ -1,7 +1,6 @@
 module.exports = function () {
     return {
         students: [],
-        
         on: function (eventName, student, callback) {
             if (this.students.indexOf(student) < 0) {
                 student.events = {};
@@ -13,10 +12,10 @@ module.exports = function () {
         off: function (eventName, student) {
             if (this.students.indexOf(student) > -1) {
                 delete student.events[eventName];
-                var steps = eventName.split(".");
+                var steps = eventName.split('.');
                 for (var event in student.events) {
-                    if (student.events.hasOwnProperty(event) && steps.length == 1 
-                        && event.indexOf(steps[0]) > -1) {
+                    if (student.events.hasOwnProperty(event) && steps.length == 1 &&
+                        event.indexOf(steps[0]) > -1) {
                         delete student.events[event];
                     }
                 }
@@ -26,7 +25,8 @@ module.exports = function () {
         emit: function (eventName) {
             var listEvents = eventName.split(".");
             var f = (student) => {
-                if (this.students.indexOf(student) > -1 && student.events.hasOwnProperty(listEvents[0])) {
+                if (this.students.indexOf(student) > -1 &&
+                    student.events.hasOwnProperty(listEvents[0])) {
                     student.events[listEvents[0]].apply(student);
                     if (listEvents.length > 1 && student.events.hasOwnProperty(eventName)) {
                         student.events[eventName].apply(student);

34a1ca56844617ad1a1328d39edd345266256f96
alexander.vakhitov <deadhunter007@live.ru>
2015-11-02 14:01:05 +0500

    Выполнено основное задание + дополнительное.

--- emitter.js
+++ emitter.js
@@ -1,23 +1,77 @@
 module.exports = function () {
-    return {
-        on: function (eventName, student, callback) {
-
-        },
-
-        off: function (eventName, student) {
-
-        },
-
-        emit: function (eventName) {
-
-        },
-
-        several: function (eventName, student, callback, n) {
-
-        },
-
-        through: function (eventName, student, callback, n) {
-
-        }
-    };
+    return new Emitter();
 };
+
+function Emitter() {
+    this.events = {};
+}
+
+Emitter.prototype.on = function on(eventName, student, callback) {
+    this._addEvent(eventName, student, callback, Infinity, 'each');
+};
+
+Emitter.prototype.off = function off(eventName, student) {
+    var foundInfo = [];
+    while (foundInfo = this._findStudent(eventName, student)) {
+        this.events[foundInfo[1]].splice(foundInfo[0], 1);
+    }
+};
+
+Emitter.prototype.emit = function emit(eventName) {
+    var events = eventName.split('.');
+    var emitEvent = '';
+    for (var i = 0; i < events.length; ++i) {
+        emitEvent += events[i];
+        this.events[emitEvent].forEach(function (item) {
+            item.callback.call(item.student);
+        });
+        emitEvent += '.';
+    }
+};
+
+Emitter.prototype.several = function several(eventName, student, callback, n) {
+    this._addEvent(eventName, student, callback, n, 'each');
+};
+
+Emitter.prototype.through = function through(eventName, student, callback, n) {
+    this._addEvent(eventName, student, callback, n, 'step');
+};
+
+Emitter.prototype._addEvent =
+    function _addEvent(eventName, student, callback, n, type) {
+        var tmpEvents = Object.keys(this.events);
+        if (tmpEvents.indexOf(eventName) < 0) {
+            this.events[eventName] = [];
+        }
+        if (this._findStudent(eventName, student)) {
+            console.error('Ошибка! Данный студент уже изучает эту дисциплину!');
+            return;
+        }
+        this.events[eventName].push(new Student(student, callback, type, n));
+    };
+
+Emitter.prototype._findStudent = function _findStudent(eventName, student) {
+    for (var event in this.events) {
+        if (this.events.hasOwnProperty(event)) {
+            if (event.indexOf(eventName) >= 0) {
+                if (this.events[event]) {
+                    var students = this.events[event];
+                    var length = this.events[event].length;
+                    for (var i = 0; i < length; ++i) {
+                        if (this.events[event][i].student === student) {
+                            return [i, event];
+                        }
+                    }
+                }
+            }
+        }
+    }
+};
+
+function Student(student, callback, type, n) {
+    this.student = student;
+    this.callback = callback;
+    this.type = type;
+    this.n = n;
+    this.counter = 0;
+}

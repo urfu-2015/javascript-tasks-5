845b30c5180f971f5dafecff97891a19f0a0b66b
KladovValery <valerijkladov@yandex.ru>
2015-11-02 22:35:19 +0400

    пока не очень, завтра поправлю

--- emitter.js
+++ emitter.js
@@ -1,23 +1,34 @@
 module.exports = function () {
+    var allEvents = [];
     return {
         on: function (eventName, student, callback) {
-
+            var eventIndex = getIndexEventName(allEvents, 'eventName', eventName);
+ 
+            return eventIndex > 0 ?
+                allEvents[eventIndex].participants.push(student)
+                :
+                allEvents.push(
+                    {
+                        eventName: eventName, 
+                        participants: [student],
+                        senceEvent: callback
+                    });
         },
 
         off: function (eventName, student) {
-
+            var indexEvent = getIndexEventName(allEvents, 'eventName', eventName);
+            return allEvents[indexEvent].participants.filter(function (name) {
+                return name !== student;
+            });
         },
 
-        emit: function (eventName) {
-
-        },
-
-        several: function (eventName, student, callback, n) {
-
-        },
-
-        through: function (eventName, student, callback, n) {
-
+    function getIndexEventName (array, property, key) {
+    for (var i = 0; i < array.length; i++) {
+        if (array[i].property === key) {
+            return i;
         }
-    };
+    }
+        return -1;
+}
+
 };

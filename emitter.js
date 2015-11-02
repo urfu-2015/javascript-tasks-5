8dbe69fef9bd3c54bc4547f4d4eb5dda74d9b2c4
Alexey Morozov <alexeymorozovv@yandex.ru>
2015-11-03 03:32:07 +0500

    add solution

--- emitter.js
+++ emitter.js
@@ -1,29 +1,67 @@
 module.exports = function () {
-
-    var events = {};
+    var subscribers = [];
     return {
         on: function (eventName, student, callback) {
-            
+            var newSubscriber = {
+                student: student,
+                callback: callback,
+                eventName: eventName,
+                checkFunction: function () {
+                    return true;
+                }
+            };
+            subscribers.push(newSubscriber);
         },
 
         off: function (eventName, student) {
-
+            subscribers = subscribers.filter(function (sub) {
+                return (!(sub.eventName.indexOf(eventName) === 0 & sub.student === student));
+            });
         },
 
         emit: function (eventName) {
-
+            subscribers.forEach(function (sub) {
+                var events = getEvents(eventName);
+                if (events.indexOf(sub.eventName) !== -1) {
+                    if (sub.checkFunction()) {
+                        sub.callback.call(sub.student);
+                    }
+                }
+            });
         },
 
         several: function (eventName, student, callback, n) {
-
+            var newSubscriber = {
+                student: student,
+                callback: callback,
+                eventName: eventName,
+                checkFunction: function () {
+                    return this.callCount++ < n;
+                },
+                callCount: 0
+            };
+            subscribers.push(newSubscriber);
         },
 
         through: function (eventName, student, callback, n) {
-
-        },
-
-        _addEvent: function _addEvent (eventName, student, callback, checkFunction) {
-            
-        } 
+            var newSubscriber = {
+                student: student,
+                callback: callback,
+                eventName: eventName,
+                checkFunction: function () {
+                    return ++this.callCount % n == 0;
+                },
+                callCount: 0
+            };
+            subscribers.push(newSubscriber);
+        }
     };
 };
+
+function getEvents(eventName) {
+    var events = eventName.split('.');
+    for (var i = 1, l = events.length; i < l; i++) {
+        events[i] = events[i - 1] + '.' + events[i];
+    }
+    return events;
+}

be5ae87c38b71d6f4b493089a1c4f220bfd1d441
eimic <korabelnikovat@gmail.com>
2015-11-02 13:56:40 +0500

    Add solution
    Постараюсь сделать лучше.

--- emitter.js
+++ emitter.js
@@ -1,19 +1,43 @@
 module.exports = function () {
+    var events = [];
     return {
         on: function (eventName, student, callback) {
-
+            events.push({name: eventName, student: student, callback: callback});
         },
 
         off: function (eventName, student) {
-
+            var changeEvents = [];
+            events.forEach(function (event) {
+                var index = event.name.indexOf(eventName);
+                if (index === -1) {
+                    changeEvents.push(event);
+                }
+            });
+            events = changeEvents;
         },
 
         emit: function (eventName) {
-
+            var namespace = [];
+            namespace.push(eventName);
+            while (true) {
+                var dote = eventName.lastIndexOf('.');
+                if (dote !== -1) {
+                    eventName = eventName.slice(0, dote);
+                    namespace.push(eventName);
+                }
+                break;
+            }
+            events.forEach(function (event) {
+                namespace.forEach(function (name) {
+                    if (event.name === name) {
+                        event.callback.call(event.student);
+                    }
+                });
+            });
         },
-
+        // Сделаю позже
         several: function (eventName, student, callback, n) {
-
+            
         },
 
         through: function (eventName, student, callback, n) {

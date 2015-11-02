5d08624d6c0d8f8cae0e7627d2871b326c2eb8e9
Даниил <Tkach@air-daniil.at.urfu.ru>
2015-11-02 10:46:27 +0500

    made Array in participants

--- emitter.js
+++ emitter.js
@@ -2,18 +2,23 @@ module.exports = function () {
     var allEvents = [];
     return {
         on: function (eventName, student, callback) {
-            return allEvents.push(
-                {
-                    eventName: eventName, 
-                    participants: student,
-                    senceEvent: callback
-                });
+            var eventIndex = getIndexEventName(allEvents, 'eventName', eventName);
+
+            return eventIndex > 0 ?
+                allEvents[eventIndex].participants.push(student)
+                :
+                allEvents.push(
+                    {
+                        eventName: eventName, 
+                        participants: [student],
+                        senceEvent: callback
+                    });
         },
 
         off: function (eventName, student) {
-            return allEvents.filter( function (event) {
-                return event.eventName !== eventName && 
-                       event.participants !== student;
+            var indexEvent = getIndexEventName(allEvents, 'eventName', eventName);
+            return allEvents[indexEvent].participants.filter(function (name) {
+                return name !== student;
             });
         },
 
@@ -23,7 +28,7 @@ module.exports = function () {
             var indexEvent;
             while (indexOneEvent > 0) {
                 oneEvent  = oneEvent.slice(0, indexOneEvent); 
-                indexEvent = getIndexEventName(allEvents, oneEvent);
+                indexEvent = getIndexEventName(allEvents,'eventName', oneEvent);
                 if (indexEvent > -1) {
                     return allEvents[indexEvent].senceEvent;
                 }
@@ -40,9 +45,9 @@ module.exports = function () {
     }
 };
 
-function getIndexEventName (array, key) {
+function getIndexEventName (array, property, key) {
     for (var i = 0; i < array.length; i++) {
-        if (array[i].eventName === key) {
+        if (array[i].property === key) {
             return i;
         }
     }

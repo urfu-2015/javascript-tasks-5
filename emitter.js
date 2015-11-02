382e4512fd5d1e2f2c88c1cf588395ec5c378dc3
Rybalka Ekaterina <lonelygopnik@gmail.com>
2015-11-02 22:42:05 +0500

    Update emitter.js

--- emitter.js
+++ emitter.js
@@ -1,15 +1,32 @@
 module.exports = function () {
     return {
+        students: [],
+        
         on: function (eventName, student, callback) {
-
+            if (this.students.indexOf(student) < 0) {
+                student.events = {};
+                this.students.push(student);
+            }
+            student.events[eventName] = callback;
         },
 
         off: function (eventName, student) {
-
+            if (student in this.students) {
+                delete student[eventName];
+            }
         },
 
         emit: function (eventName) {
-
+            var listEvents = eventName.split(".");
+            var f = (student) => {
+                if (this.students.indexOf(student) > -1 && student.events.hasOwnProperty(listEvents[0])) {
+                    student.events[listEvents[0]].apply(student);
+                    if (listEvents.length > 1 && student.events.hasOwnProperty(eventName)) {
+                        student.events[eventName].apply(student);
+                    }
+                }
+            };
+            this.students.forEach(f);
         },
 
         several: function (eventName, student, callback, n) {

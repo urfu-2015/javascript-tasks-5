7fc06708c45634ffda970c5d99cff367dd5cdfe2
v-samoylov <v-samoylov@users.noreply.github.com>
2015-11-02 20:15:38 +0500

    Update emitter.js

--- emitter.js
+++ emitter.js
@@ -1,15 +1,31 @@
 module.exports = function () {
+    var events = []
     return {
         on: function (eventName, student, callback) {
-
+            if (!events[eventName]) {
+                events[eventName] = [];
+            }
+            events[eventName].push({student, callback});
         },
 
         off: function (eventName, student) {
-
+            if (events[eventName]) {
+                for (var i = 0; i < events[eventName].length; i++) {
+                    if (student === events[eventName][i]["student"]){
+                        events[eventName].splice(i, 1);
+                        i--;
+                        break;
+                    }
+                }
+            }
         },
 
         emit: function (eventName) {
-
+            if (events[eventName]) {
+                for (var i = 0; i < events[eventName].length; i++) {
+                    events[eventName][i]["callback"].call(events[eventName][i]["student"]);
+                }
+            }
         },
 
         several: function (eventName, student, callback, n) {

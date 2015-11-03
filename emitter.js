587e690b01c3ccc4bb50d7630548b7969ef17621
NekrasovAlexey <creepy404nf@gmail.com>
2015-11-03 15:29:07 +0500

    Добавил второй уровень

--- emitter.js
+++ emitter.js
@@ -3,29 +3,53 @@ module.exports = function () {
         emitOn: {},
         counters: {},
         on: function (eventName, student, callback) {
+            var levelEvents = eventName.split('.');
+            if (levelEvents.length > 1) {
+                this.on(levelEvents[0] + levelEvents[1], student, callback);
+                return;
+            }
             var studentsOn = this.emitOn[eventName];
             var newStudent = {student: student,
-                              callback: callback};
-            if (studentsOn) {
-                studentsOn.push(newStudent);
-            } else {
+                callback: callback};
+            if (!studentsOn) {
                 this.emitOn[eventName] = [newStudent];
+                return;
+            }
+            if (studentsOn.indexOf(newStudent) < 0) {
+                studentsOn.push(newStudent);
             }
         },
         off: function (eventName, student) {
-            var studentsOn = this.emitOn[eventName];
-            if (studentsOn) {
-                this.emitOn[eventName] = studentsOn.filter(function (studentOn) {
-                    return studentOn.student != student;
-                });
+            var levelEvents = eventName.split('.');
+            if (levelEvents.length > 1) {
+                this.off(levelEvents[0] + levelEvents[1], student);
+                return;
             }
+            var emitEvents = Object.keys(this.emitOn);
+            emitEvents = emitEvents.filter(function (event) {
+                return event.search(eventName) > -1;
+            });
+            emitEvents.forEach(function (eventName) {
+                var studentsOn = this.emitOn[eventName];
+                if (studentsOn) {
+                    this.emitOn[eventName] = studentsOn.filter(function (studentOn) {
+                        return studentOn.student != student;
+                    });
+                }
+            }.bind(this));
         },
         emit: function (eventName) {
+            var levelEvents = eventName.split('.');
+            if (levelEvents.length > 1) {
+                this.emit(levelEvents[0]);
+                this.emit(levelEvents[0] + levelEvents[1]);
+                return;
+            }
             var studentsOn = this.emitOn[eventName];
             if (studentsOn) {
                 studentsOn.forEach(function (student) {
                     student.callback.call(student.student);
-                })
+                });
             }
         },
         several: function (eventName, student, callback, n) {

881352aa763ac8d64d7389c8d7f64dcf8838fceb
Kate Latypova <katelat3009@gmail.com>
2015-11-03 13:46:07 +0500

    update after first feedback

--- emitter.js
+++ emitter.js
@@ -17,56 +17,60 @@ module.exports = function () {
                 events[eventName] = [];
             }
             events[eventName] = events[eventName].concat(newReaction);
-            return events[eventName][events[eventName].length - 1];
+            return newReaction;
         },
 
         off: function (eventName, student) {
-            var deleteEvents = [eventName];
             var studentNumber = students.indexOf(student);
-            if (eventName.indexOf('.') === -1) {
-                deleteEvents = deleteEvents.concat(Object.keys(events).filter(function (event) {
-                    if (event.indexOf(eventName) === -1 || event === eventName) {
-                        return false;
-                    }
-                    return true;
-                }));
-            }
-            for (var event = 0; event < deleteEvents.length; event++) {
-                for (var i = 0; i < events[deleteEvents[event]].length; i++) {
-                    if (events[deleteEvents[event]][i].student === studentNumber) {
-                        events[deleteEvents[event]].splice(i, 1);
+            if (eventName in events && studentNumber !== -1) {
+                var deleteEvents = [eventName];
+                if (eventName.indexOf('.') === -1) {
+                    deleteEvents = deleteEvents.concat(Object.keys(events).filter(function (event) {
+                        if (event.indexOf(eventName) === -1 || event === eventName) {
+                            return false;
+                        }
+                        return true;
+                    }));
+                }
+                for (var event = 0; event < deleteEvents.length; event++) {
+                    for (var i = 0; i < events[deleteEvents[event]].length; i++) {
+                        if (events[deleteEvents[event]][i].student === studentNumber) {
+                            events[deleteEvents[event]].splice(i, 1);
+                        }
                     }
                 }
             }
         },
 
         emit: function (eventName) {
-            var currentEvents = eventName.split('.');
-            if (currentEvents.length > 1) {
-                currentEvents.splice(1, 1, eventName);
+            var currentEvents = [eventName];
+            var indexOfPoint = eventName.lastIndexOf('.');
+            while (indexOfPoint !== -1) {
+                eventName = eventName.substring(0, indexOfPoint);
+                currentEvents = currentEvents.concat(eventName);
+                indexOfPoint = eventName.lastIndexOf('.');
             }
-            currentEvents = currentEvents.reverse();
             currentEvents.forEach(function (event) {
-                if (event in events) {
-                    events[event].forEach(function (person) {
-                        var keys = Object.keys(person);
-                        if (keys.length == 2) {
-                            person.callback.call(students[person.student]);
-                        } else {
-                            person.currentReaction++;
-                            if (keys.indexOf('countOfReactions') !== -1 &&
-                                person.currentReaction <= person.countOfReactions) {
-                                person.callback.call(students[person.student]);
-                            }
-                            if (keys.indexOf('numberOfReaction') !== -1) {
-                                if (person.currentReaction === person.numberOfReaction) {
-                                    person.callback.call(students[person.student]);
-                                    person.currentReaction = 0;
-                                }
-                            }
-                        }
-                    });
+                if (!(event in events)) {
+                    return;
                 }
+                events[event].forEach(function (person) {
+                    var keys = Object.keys(person);
+                    if (keys.length == 2) {
+                        person.callback.call(students[person.student]);
+                    } else {
+                        person.currentReaction++;
+                        if (keys.indexOf('countOfReactions') !== -1 &&
+                            person.currentReaction <= person.countOfReactions) {
+                            person.callback.call(students[person.student]);
+                        }
+                        if (keys.indexOf('numberOfReaction') !== -1 &&
+                            person.currentReaction === person.numberOfReaction) {
+                            person.callback.call(students[person.student]);
+                            person.currentReaction = 0;
+                        }
+                    }
+                });
             });
         },
 

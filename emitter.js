1cc51ec86dd7f6e5a68b7b489c2f36b3c7c8032f
Vedmax <max.vedernikov@gmail.com>
2015-11-04 22:14:10 +0500

    make stars

--- emitter.js
+++ emitter.js
@@ -1,10 +1,11 @@
 module.exports = function () {
     var names = [];
-    var studentsSubscriptions = [];
+    var studentsSub = [];
+    var limit = {};
 
     function getIndex(name) {
-        for (var i = 0; i < studentsSubscriptions.length; i++) {
-            if (studentsSubscriptions[i]['name'] === name) {
+        for (var i = 0; i < studentsSub.length; i++) {
+            if (studentsSub[i]['name'] === name) {
                 return i;
             }
         }
@@ -35,22 +36,37 @@ module.exports = function () {
     return {
         on: function (eventName, student, callback) {
             if (names.indexOf(student) !== -1) {
-                studentsSubscriptions[getIndex(student)]['actions'][eventName] = callback;
+                if (!('eventName' in studentsSub[getIndex(student)]['actions'])) {
+                    studentsSub[getIndex(student)]['actions'][eventName] = {
+                        func: callback,
+                        period: 1,
+                        count: 1,
+                        maxCount: Number.MAX_SAFE_INTEGER
+                    };
+                } else {
+                    studentsSub[getIndex(student)]['actions'][eventName]['func'] = callback;
+                }
+
             } else {
                 names.push(student);
                 var studentActions = {};
-                studentActions[eventName] = callback;
-                studentsSubscriptions.push({ name: student, actions: studentActions});
+                studentActions[eventName] = {
+                    func: callback,
+                    period: 1,
+                    count: 1,
+                    maxCount: Number.MAX_SAFE_INTEGER
+                };
+                studentsSub.push({ name: student, actions: studentActions});
             }
         },
 
         off: function (eventName, student) {
             var index = getIndex(student);
             if (index !== -1) {
-                var actionsNames = Object.keys(studentsSubscriptions[index]['actions']);
+                var actionsNames = Object.keys(studentsSub[index]['actions']);
                 for (var i = 0; i < actionsNames.length; i++) {
                     if (actionsNames[i].indexOf(eventName) !== -1) {
-                        delete studentsSubscriptions[index]['actions'][actionsNames[i]];
+                        delete studentsSub[index]['actions'][actionsNames[i]];
                     }
                 }
             }
@@ -58,23 +74,33 @@ module.exports = function () {
 
         emit: function (eventName) {
             var prefixes = getPrefixes(eventName);
-            for (var index in studentsSubscriptions) {
-                var student = studentsSubscriptions[index];
+            for (var index in studentsSub) {
+                var student = studentsSub[index];
                 var events = Object.keys(student.actions);
                 for (var i = 0; i < events.length; i++) {
                     if (isInPrefixes(prefixes, events[i])) {
-                        student.actions[events[i]].call(student['name']);
+                        if (student.actions[events[i]]['count'] ===
+                            student.actions[events[i]]['maxCount']) {
+                            this.off(events[i], student['name']);
+                            return;
+                        }
+                        if (student.actions[events[i]]['count']++ %
+                            student.actions[events[i]]['period'] === 0) {
+                            student.actions[events[i]]['func'].call(student['name']);
+                        }
                     }
                 }
             }
         },
 
         several: function (eventName, student, callback, n) {
-
+            this.on(eventName, student, callback);
+            studentsSub[getIndex(student)]['actions'][eventName]['maxCount'] = n;
         },
 
         through: function (eventName, student, callback, n) {
-
+            this.on(eventName, student, callback);
+            studentsSub[getIndex(student)]['actions'][eventName]['period'] = n;
         }
     };
 };

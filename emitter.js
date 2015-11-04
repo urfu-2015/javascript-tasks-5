818f59261c2f9e8d549b4f0af612562367d2d277
nikitc <nikitc6@rambler.ru>
2015-11-04 14:06:06 +0500

    Update

--- emitter.js
+++ emitter.js
@@ -1,37 +1,61 @@
 
 module.exports = function () {
-    var eventsOfStudents = [];
     return {
+        eventsOfStudents: [],
         on: function (eventName, student, callback) {
             var event = {
                 name: eventName,
                 student: student,
                 callback: callback
             };
-            eventsOfStudents.push(event);
+            this.eventsOfStudents.push(event);
 
         },
 
         off: function (eventName, student) {
-            var newEventsOfStudents = [];
-            eventsOfStudents.forEach(function (event) {
-                if (event.name.indexOf(eventName) < 0 || student != event.student) {
-                    newEventsOfStudents.push(event);
+                for (var i = 0; i < this.eventsOfStudents.length; i++) {
+                    if (this.eventsOfStudents[i].name.indexOf(eventName) >= 0 || student === this.eventsOfStudents[i].student) {
+                        this.eventsOfStudents.splice(i, 1);
                     }
-                });
-            eventsOfStudents = newEventsOfStudents.slice();
+                }
         },
 
         emit: function (eventName) {
-            eventsOfStudents.forEach(function (student) {
-                if (student.name === eventName) {
-                    student.callback.call(student);
+            function createEvents() {
+                var eventCurrent = eventName.split('.');
+                if (eventCurrent.length === 1) {
+                    return [eventName];
+                } else {
+                    var nameEvent = eventCurrent[0];
+                    var events = [nameEvent];
+                    for (var i = 1; i < eventCurrent.length; i++) {
+                        nameEvent = nameEvent + '.' + eventCurrent[i];
+                        events.push(nameEvent);
+                    }
+                    return events;
                 }
-                var mainEvent = eventName.split('.')[0];
-                if (mainEvent !== eventName && student.name === eventName) {
-                    student.callback.call(student);
+            }
+            if (this.is_exist_name(eventName, this.eventsOfStudents)) {
+                var events = createEvents();
+                for (var i = 0; i < events.length; i++) {
+                    for (var j = 0; j < this.eventsOfStudents.length; j++) {
+                        var current = this.eventsOfStudents[j];
+                        if (events[i] === current.name){
+                            var callback = current.callback;
+                            callback.call(this.eventsOfStudents[j].student);
+                        }
+                    }
                 }
-            });
+            }
+        },
+        is_exist_name: function(eventName, events) {
+            for (var i = 0; i < events.length; i++) {
+                if (eventName === events[i].name) {
+                    return true;
+                }
+            }
+            return false;
+
         },
 
         several: function (eventName, student, callback, n) {

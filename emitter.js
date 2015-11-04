09347d066a2595e81f57c393db43258d5a65b3b3
Edelweiss603111 <arinavaruhina@gmail.com>
2015-11-04 17:00:55 +0500

    Исправила все ошибки

--- emitter.js
+++ emitter.js
@@ -2,53 +2,43 @@ module.exports = function () {
     return {
         events: {},
         on: function (eventName, student, callback) {
-            if (this.events[eventName] !== undefined) {
-                this.events[eventName].push({student: student, callback: callback});
-            } else {
-                this.events[eventName] = [{student: student, callback: callback}];
-            }
+            this.events[eventName] = this.events[eventName] || [];
+            this.events[eventName].push({student: student, callback: callback});
         },
 
         off: function (eventName, student) {
-            var event = this.events[eventName];
-            if (event !== undefined) {
-                for (var i = 0; i < event.length; i++) {
-                    if (event[i].student === student) {
-                        event = event.slice(0,
-                                event.indexOf(event[i])).concat(event.slice(event.indexOf(event[i])+1,
-                                event.length))
-                        this.events[eventName] = event;
-                        return;
+            if (!this.events[eventName]) {
+                return;
+            }
+            var keys = Object.keys(this.events);
+            for (var i = 0; i < keys.length; i++) {
+                if (keys[i].indexOf(eventName) != -1) {
+                    for (var j = 0; j < this.events[keys[i]].length; j++) {
+                        if (this.events[keys[i]][j].student === student) {
+                            delete this.events[keys[i]][j];
+                        }
                     }
                 }
             }
         },
 
         emit: function (eventName) {
-            var indexOfEvent = Object.keys(this.events).indexOf(eventName);
-            if (indexOfEvent !== -1) {
-                var eventNames = '';
-                var names = [];
-                if (eventName.indexOf('.') != -1) {
-                    var names = [eventName];
+            var keys = Object.keys(this.events);
+            var names = [];
+            names = [eventName];
+            if (eventName.indexOf('.') != -1) {
+                names.push(eventName.slice(0, eventName.indexOf('.')))
+            }
+            var events = this.events;
+            names.forEach(function (name) {
+                for (var i = 0; i < keys.length; i++) {
+                    if (keys[i] === name) {
+                        callFunction(name, events);
                     }
-                while (eventName.indexOf('.') != -1) {
-                    eventNames = eventName.slice(0,
-                                eventName.indexOf('.'))+":"+(eventName.slice(eventName.indexOf('.')+1,
-                                eventName.length))
-                    eventName = eventName.slice(eventName.indexOf('.')+1,
-                                eventName.length)
                 }
-                names.push(eventNames.split(':'));
-                names.forEach(function(eventName) {
-                        var event = this.events[eventName];
-                        for (var i = 0; i < event.length; i++) {
-                            event[i].callback.call(event[i].student);
-                        }
-                    });
-                }
-            
+            });
         },
+        
 
         several: function (eventName, student, callback, n) {
 
@@ -59,4 +49,10 @@ module.exports = function () {
         }
     };
 };
-
+function callFunction(eventName, events) {
+    events[eventName].forEach(function (student) {
+        if (student !== undefined) {
+            student.callback.call(student.student);
+        }
+    });
+}

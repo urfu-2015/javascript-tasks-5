a325bbd36a7e2aed4ba5858f1778a26117b972e4
davletova <davletova.a.g@gmail.com>
2015-11-05 18:51:27 +0500

    Update emitter.js

--- emitter.js
+++ emitter.js
@@ -1,37 +1,49 @@
 module.exports = function () {
 	var events_subscribers = {};
-	
+	var students = [];
     return {
         on: function (eventName, student, callback) {
-			student[eventName] = callback;
-			if (!events_subscribers.hasOwnProperty(eventName)) {
-				events_subscribers[eventName] = [];
+			var i = students.indexOf(student);
+			if (i < 0) {
+				i = students.length;
+				students[i] = student;
 			}
-			events_subscribers[eventName].push(student);
+			if (!events_subscribers.hasOwnProperty(eventName)) {
+				events_subscribers[eventName] = {};
+			}
+			events_subscribers[eventName][i] = callback;
         },
 
         off: function (eventName, student) {
-			delete student[eventName];
-			var i = events_subscribers[eventName].indexOf(student);
-			if (i >= 0) {
-				events_subscribers[eventName].splice(i,1);
+			var delSubscriber = function(i, evName) {
+				if (events_subscribers.hasOwnProperty(evName) && events_subscribers[evName].hasOwnProperty(i)) {
+					delete events_subscribers[evName][i];
+				}
 			}
+			var i = students.indexOf(student);
+			if (i >= 0) {
+				for(var event in events_subscribers) {
+					if (event.indexOf(eventName) >= 0) {
+						delSubscriber(i,event);
+					}
+				}
+			}
+			return events_subscribers;
         },
 
         emit: function (eventName) {
-			var events = [];
-			events.push(eventName)
-			var subEventName = eventName.split('.')[0];
-			if (!(subEventName === eventName)) {
-				events.push(subEventName);
-			}
-			for (var j = 0; j < events.length; j++) {
-				if (events_subscribers.hasOwnProperty(events[j])) {
-				var subscribers = events_subscribers[events[j]];
-				for (var i = 0; i < subscribers.length; i++) {
-					subscribers[i][events[j]].call(subscribers[i]);
+			var perform = function(evName) {
+				if (events_subscribers.hasOwnProperty(evName)) {
+					var subscribers = events_subscribers[evName];
+					for(var numSt in subscribers) {
+						subscribers[numSt].call(students[numSt]);
 					}
 				}
+			}
+			perform(eventName);
+			var subEventName = eventName.split('.')[0];
+			if (!(subEventName === eventName)) {
+				perform(subEventName);
 			}
         },
 

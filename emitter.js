57372d4ee642e1628822fd5aa5798dc3ac51c256
nikitc <nikitc6@rambler.ru>
2015-11-05 22:01:26 +0500

    Update 6

--- emitter.js
+++ emitter.js
@@ -1,63 +1,49 @@
 
 module.exports = function () {
     return {
-        eventsOfStudents: [],
+        eventsOfStudents: {},
         on: function (eventName, student, callback) {
-            var event = {
-                name: eventName,
-                student: student,
-                callback: callback
-            };
-            this.eventsOfStudents.push(event);
+            if (!(eventName in this.eventsOfStudents)) {
+                this.eventsOfStudents[eventName] = [];
+                }
+            this.eventsOfStudents[eventName].push({student: student, callback: callback});
 
         },
 
         off: function (eventName, student) {
-            for (var i = 0; i < this.eventsOfStudents.length; i++) {
-                if (this.eventsOfStudents[i].name.indexOf(eventName) >= 0 &&
-                    student === this.eventsOfStudents[i].student) {
-                    this.eventsOfStudents.splice(i, 1);
-                    i--;
-                }
-            }
-        },
-
-        emit: function (eventName) {
-            function createEvents() {
-                var eventCurrent = eventName.split('.');
-                if (eventCurrent.length === 1) {
-                    return [eventName];
-                } else {
-                    var nameEvent = eventCurrent[0];
-                    var events = [nameEvent];
-                    for (var i = 1; i < eventCurrent.length; i++) {
-                        nameEvent = nameEvent + '.' + eventCurrent[i];
-                        events.push(nameEvent);
-                    }
-                    return events;
-                }
-            }
-            if (this.is_exist_name(eventName, this.eventsOfStudents)) {
-                var events = createEvents();
-                for (var i = 0; i < events.length; i++) {
-                    for (var j = 0; j < this.eventsOfStudents.length; j++) {
-                        var current = this.eventsOfStudents[j];
-                        if (events[i] === current.name) {
-                            var callback = current.callback;
-                            callback.call(this.eventsOfStudents[j].student);
+            for (event in this.eventsOfStudents) {
+                if (event.indexOf(eventName) >= 0) {
+                    for (var i = 0; i < this.eventsOfStudents[event].length; i++) {
+                        if (this.eventsOfStudents[event][i].student === student) {
+                            this.eventsOfStudents.splice(i, 1);
+                            i--;
                         }
                     }
                 }
             }
         },
-        is_exist_name: function (eventName, events) {
-            for (var i = 0; i < events.length; i++) {
-                if (eventName === events[i].name) {
-                    return true;
+
+        emit: function (eventName) {
+            if (eventName in this.eventsOfStudents) {
+                var events = this.createEvents();
+                for (var i = 0; i < events.length; i++) {
+                    for (var j = 0; j < this.eventsOfStudents[events[i]].length; j++) {
+                        var current = this.eventsOfStudents[events[i]][j];
+                            var callback = current.callback;
+                            callback.call(this.eventsOfStudents[j].student);
+                    }
                 }
             }
-            return false;
-
+        },
+        createEvents: function () {
+            var eventsList = eventName.split('.');
+            var nameEvent = eventsList[0];
+            var events = [nameEvent];
+            for (var i = 1; i < eventsList.length; i++) {
+                nameEvent = nameEvent + '.' + eventsList[i];
+                events.push(nameEvent);
+            }
+            return events;
         },
 
         several: function (eventName, student, callback, n) {

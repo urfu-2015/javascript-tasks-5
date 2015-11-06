53a78965a0d831f49ac753f59c06f3ddbc0780a1
Даниил <Tkach@MacBook-Air-Daniil.local>
2015-11-06 23:40:30 +0500

    I MADE IT

--- emitter.js
+++ emitter.js
@@ -1,63 +1,59 @@
 module.exports = function () {
-    var allEvents = [];
+    var students = [];
     return {
         on: function (eventName, student, callback) {
-            var eventIndex = getIndexEventName(allEvents, eventName);
-
-            return eventIndex > -1 ?
-                allEvents[eventIndex].participants.push(student)
-                :
-                allEvents.push(
+            var studentIndex = getIndexStudentName(students, student);
+            return studentIndex > -1 ?
+                students[studentIndex].events.push(
                     {
-                        eventName: eventName, 
-                        participants: [student],
-                        senceEvent: callback
+                        eventName: eventName,
+                        func: callback
+                    })
+                :
+                students.push(
+                    {
+                        studentName: student,
+                        events: [
+                        {
+                            eventName: eventName,
+                            func: callback
+                        }]
                     });
         },
 
         off: function (eventName, student) {
-            for (var i = 0; i < allEvents.length; i++) {
-                if (allEvents[i].eventName.indexOf(eventName) > -1) {
-                    allEvents[i].participants = allEvents[i].participants.filter(function (name) {
-                    return name !== student;
+            for (var i = 0; i < students.length; i++) {
+                if (students[i].studentName === student) {
+                    students[i].events = students[i].events.filter(function (event) {
+                        return event.eventName.indexOf(eventName) === -1;
                     });
                 }
             }
-            return allEvents;
          },
 
         emit: function (eventName) {
-            var listEvent = eventName.split('.');
-            var countEventName = listEvent[0];
-            for (var i = 0; i < listEvent.length; i++) {
-                if (i > 0) {
-                    countEventName += '.' + listEvent[i];
-                }
-                indexEvent = getIndexEventName(allEvents, countEventName);
-                if (indexEvent > -1) {
-                    var callBackEvent = allEvents[indexEvent].senceEvent;
-                    allEvents[indexEvent].participants.forEach(function (elem) {
-                        callBackEvent.call(elem);
+            students.forEach(function (student) {
+                var listEvent = eventName.split('.');
+                var oneEvent = listEvent[0]
+                for (var i = 0; i < listEvent.length; i++) {
+                    if (i > 0) {
+                    oneEvent += '.' + listEvent[i];
+                    }
+                    student.events.forEach(function (event) {
+                    if (event.eventName === oneEvent) {
+                        var callBack = event.func;
+                        callBack.apply(student.studentName);
+                        }
                     });
                 }
-            };
-        }
-    };
-}
-
-function checkHaveEventName (array, eventName, student) {
-    for (var i = 0; i < array.length; i++) {
-        if (array[i].eventName.indexOf(eventName) > -1) {
-            array[i].eventName.participants = array[i].eventName.participants.filter(function (name) {
-                return name !== student;
             });
         }
     }
 }
 
-function getIndexEventName (array, key) {
+function getIndexStudentName (array, key) {
     for (var i = 0; i < array.length; i++) {
-        if (array[i].eventName === key) {
+        if (array[i].studentName === key) {
             return i;
         }
     }

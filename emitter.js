78d8b452dd98121b4a1767209d26c01bb9be3cea
Onufrienko Ekaterina <kat.onuf@yandex.ru>
2015-11-06 22:51:31 +0500

    Переписала

--- emitter.js
+++ emitter.js
@@ -1,63 +1,45 @@
 module.exports = function () {
-    var students = [];
+    events = [];
     return {
         on: function (eventName, student, callback) {
-            var index = this.findIndex(student);
-            if (index === -1) {
-                var studentEvents = [];
-                studentEvents[eventName] = callback;
-                var newStudent = {
-                    description: student,
-                    events: studentEvents,
-                    isUsuall: true
-                };
-                students.push(newStudent);
-            } else {
-                students[index].events[eventName] = callback;
-            }
+            this.add(eventName, student, callback, Infinity, 'on');
         },
 
         off: function (eventName, student) {
-            var index = this.findIndex(student);
-            if (index != -1) {
-                var events = Object.keys(students[index].events);
-                var toDelete = events.filter(function (ev) {
-                    var isEqual = ev === eventName;
-                    var isContained = ev.indexOf(eventName) != -1;
-                    return isEqual || isContained;
-                });
-                toDelete.forEach(function (ev) {
-                    delete students[index].events[ev];
-                });
+            var index = this.findIndex(eventName, student);
+            while (index > -1) {
+                events.splice(index, 1);
+                index = this.findIndex(eventName, student);
             }
         },
 
         emit: function (eventName) {
             var namespace = this.getNamespace(eventName);
-            students.forEach(function (student) {
+            events.forEach(function (eventRecord) {
                 namespace.forEach(function (name) {
-                    if (student.hasOwnProperty('isUsuall')) {
-                        if (Object.keys(student.events).indexOf(name) != -1) {
-                            student.events[name].apply(student.description);
-                        }
-                    } else {
-                        var counts = student.events[name].counts;
-                        var called = student.events[name].called;
-                        var mod = -1;
-                        if (called > 0) {
-                            mod = called % counts;
-                        }
-                        if (student.hasOwnProperty('isThrough') && student.isThrough) {
-                            if (mod === 0) {
-                                student.events[name].apply(student.description);
-                            }
-                            student.events[name].called++;
-                        }
-                        if (student.hasOwnProperty('isSeveral') && student.isSeveral) {
-                            if (counts >= called) {
-                                student.events[name].apply(student.description);
-                                student.events[name].called++;
-                            }
+                    if (eventRecord.eventName === name) {
+                        var called = eventRecord.calledTimes;
+                        var counts = eventRecord.canBeCalled;
+                        switch (eventRecord.subscription) {
+                            case 'on':
+                                eventRecord.action.apply(eventRecord.student);
+                                break;
+                            case 'several':
+                                if (called <= counts) {
+                                    eventRecord.action.apply(eventRecord.student);
+                                    eventRecord.calledTimes++;
+                                }
+                                break;
+                            case 'through':
+                                var mod = -1;
+                                if (called > 0) {
+                                    mod = called % counts;
+                                }
+                                if (mod === 0) {
+                                    eventRecord.action.apply(eventRecord.student);
+                                }
+                                eventRecord.calledTimes++;
+                                break;
                         }
                     }
                 });
@@ -65,32 +47,36 @@ module.exports = function () {
         },
 
         several: function (eventName, student, callback, n) {
-            this.add(eventName, student, callback, n, true, false);
+            this.add(eventName, student, callback, n, 'several');
         },
 
         through: function (eventName, student, callback, n) {
-            this.add(eventName, student, callback, n, false, true);
+            this.add(eventName, student, callback, n, 'through');
         },
 
-        add: function (eventName, student, callback, n, isSeveral, isThrough) {
-            var index = this.findIndex(student);
-            if (index === -1) {
-                var studentEvents = [];
-                studentEvents[eventName] = callback;
-                studentEvents[eventName].counts = n;
-                studentEvents[eventName].called = 1;
-                var newStudent = {
-                    description: student,
-                    events: studentEvents,
-                    isSeveral: isSeveral,
-                    isThrough: isThrough
-                };
-                students.push(newStudent);
-            } else {
-                students[index].events[eventName] = callback;
-                students[index].events[eventName].counts = n;
-                students[index].events[eventName].called = 1;
+        add: function (eventName, student, callback, n, subscription) {
+            var newEvent = {
+                student: student,
+                eventName: eventName,
+                action: callback,
+                canBeCalled: n,
+                calledTimes: 0,
+                subscription: subscription
+            };
+            if (subscription === 'through') {
+                newEvent.calledTimes = 1;
             }
+            events.push(newEvent);
+        },
+
+        findIndex: function (eventName, student) {
+            for (var i = 0; i < events.length; i++) {
+                if (events[i].student == student &&
+                events[i].eventName.indexOf(eventName) != -1) {
+                    return i;
+                }
+            }
+            return -1;
         },
 
         getNamespace: function (eventName) {
@@ -105,15 +91,6 @@ module.exports = function () {
                 namespace.push(eventName);
             }
             return namespace;
-        },
-
-        findIndex: function (student) {
-            for (var i = 0; i < students.length; i++) {
-                if (students[i].description == student) {
-                    return i;
-                }
-            }
-            return -1;
         }
     };
 };

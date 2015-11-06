9e3e224a02f368d333b0a8c41ed13117c06ea76a
Victoria-Vladimirova <wiktorija.wladimirowa@gmail.com>
2015-11-07 00:10:25 +0500

    Исправлены замечания

--- emitter.js
+++ emitter.js
@@ -4,115 +4,109 @@ module.exports = function () {
 
 function Lecturer() {
 
-    var listeners = {};
+    this._listeners = {};
 
-    var checkType = function (variable, type) {
-        if (typeof variable !== type) {
-            throw Error('Аргумент должен иметь тип ' + type);
-        }
-    };
+}
 
-    var getListenerLevel = function (eventName) {
-        var events = eventName.split('.');
-        var listenerLevel = listeners;
-        events.forEach(function (event, number) {
-            if (number !== 0) {
-                if (listenerLevel.nested === undefined) {
-                    listenerLevel.nested = {};
+Lecturer.prototype._checkPositiveNumber = function (variable) {
+    if (typeof variable !== 'number' || variable <= 0) {
+        throw Error('Аргумент должен быть положительным числом');
+    }
+};
+
+Lecturer.prototype._checkObject = function (variable) {
+    if (variable === null || typeof variable !== 'object') {
+        throw Error('Аргумент должен быть объектом');
+    }
+};
+
+Lecturer.prototype._checkFunction = function (variable) {
+    if (typeof variable !== 'function') {
+        throw Error('Аргумент должен быть функцией');
+    }
+};
+
+Lecturer.prototype._checkNonEmptyString = function (variable) {
+    if (typeof variable !== 'string' || variable.length === 0) {
+        throw Error('Аргумент должен быть непустой строкой');
+    }
+};
+
+Lecturer.prototype._getListenersForEvent = function (eventName) {
+    var listeners = this._listeners[eventName];
+    if (listeners === undefined) {
+        listeners = this._listeners[eventName] = [];
+    }
+    return listeners;
+};
+
+Lecturer.prototype.on = function (eventName, student, callback) {
+    this._checkNonEmptyString(eventName);
+    this._checkObject(student);
+    this._checkFunction(callback);
+    this._getListenersForEvent(eventName).push(new Listener(student, callback));
+};
+
+Lecturer.prototype.off = function (eventName, student) {
+    this._checkNonEmptyString(eventName);
+    this._checkObject(student);
+    Object.keys(this._listeners).forEach(function (event) {
+        if (event.indexOf(eventName) === 0) {
+            this._listeners[event].forEach(function (listener, number) {
+                if (listener.getStudent() === student) {
+                    delete this._listeners[event][number];
                 }
-                listenerLevel = listenerLevel.nested;
-            }
-            if (listenerLevel[event] === undefined) {
-                listenerLevel[event] = {};
-            }
-            listenerLevel = listenerLevel[event];
-        });
-        if (listenerLevel.listeners === undefined) {
-            listenerLevel.listeners = [];
-        }
-        return listenerLevel;
-    };
-
-    this.on = function (eventName, student, callback) {
-        checkType(eventName, 'string');
-        checkType(student, 'object');
-        checkType(callback, 'function');
-        getListenerLevel(eventName).listeners.push(new Listener(student, callback));
-    };
-
-    this.off = function (eventName, student) {
-        checkType(eventName, 'string');
-        checkType(student, 'object');
-        var listenerLevel = getListenerLevel(eventName);
-        listenerLevel.listeners.forEach(function (listener, number) {
-            if (listener.student === student) {
-                delete listenerLevel.listeners[number];
-            }
-        });
-        if (listenerLevel.nested) {
-            Object.keys(listenerLevel.nested).forEach(function (event) {
-                this.off(eventName + '.' + event, student);
             }.bind(this));
         }
-    };
+    }.bind(this));
+};
 
-    this.emit = function (eventName) {
-        checkType(eventName, 'string');
-        var events = eventName.split('.');
-        var listenerLevel = listeners;
-        events.forEach(function (event, number) {
-            if (number !== 0) {
-                if (listenerLevel.nested === undefined) {
-                    listenerLevel.nested = {};
-                }
-                listenerLevel = listenerLevel.nested;
-            }
-            if (listenerLevel[event] === undefined) {
-                listenerLevel[event] = {};
-            }
-            listenerLevel = listenerLevel[event];
-            if (listenerLevel.listeners === undefined) {
-                listenerLevel.listeners = [];
-            }
-            listenerLevel.listeners.forEach(function (listener) {
-                listener.callListener();
-            });
-        });
-    };
+Lecturer.prototype.emit = function (eventName) {
+    var lastDotIdx = eventName.lastIndexOf('.');
+    if (lastDotIdx >= 0) {
+        this.emit(eventName.substring(0, lastDotIdx));
+    }
+    this._getListenersForEvent(eventName).forEach(function (listener) {
+        listener.callListener();
+    });
+};
 
-    this.several = function (eventName, student, callback, n) {
-        checkType(eventName, 'string');
-        checkType(student, 'object');
-        checkType(callback, 'function');
-        checkType(n, 'number');
-        getListenerLevel(eventName).listeners.push(new Listener(student, callback, n));
-    };
+Lecturer.prototype.several = function (eventName, student, callback, n) {
+    this._checkNonEmptyString(eventName);
+    this._checkObject(student);
+    this._checkFunction(callback);
+    this._checkPositiveNumber(n);
+    this._getListenersForEvent(eventName).push(new Listener(student, callback, n));
+};
 
-    this.through = function (eventName, student, callback, n) {
-        checkType(eventName, 'string');
-        checkType(student, 'object');
-        checkType(callback, 'function');
-        checkType(n, 'number');
-        getListenerLevel(eventName).listeners.push(new Listener(student, callback, undefined, n));
-    };
-}
+Lecturer.prototype.through = function (eventName, student, callback, n) {
+    this._checkNonEmptyString(eventName);
+    this._checkObject(student);
+    this._checkFunction(callback);
+    this._checkPositiveNumber(n);
+    this._getListenersForEvent(eventName).push(new Listener(student, callback, undefined, n));
+};
 
 
 function Listener(student, callback, maxCalls, multiple) {
 
-    this.student = student;
-    this.callback = callback;
-    this.maxCalls = maxCalls;
-    this.multiple = multiple;
+    this._student = student;
+    this._callback = callback;
+    this._maxCalls = maxCalls;
+    this._multiple = multiple;
+    this._callCount = 0;
 
-    var callCount = 0;
-
-    this.callListener = function () {
-        callCount++;
-        if ((this.maxCalls && this.maxCalls >= callCount) ||
-            (this.multiple && callCount % this.multiple == 0) ||
-            (this.maxCalls === undefined && this.multiple === undefined)) {
-            this.callback.call(this.student);
-        }
-    };
 }
+
+Listener.prototype.callListener = function () {
+    this._callCount++;
+    if ((this._maxCalls && this._maxCalls >= this._callCount) ||
+        (this._multiple && this._callCount % this._multiple === 0) ||
+        (this._maxCalls === undefined && this._multiple === undefined)) {
+        this._callback.call(this._student);
+    }
+};
+
+Listener.prototype.getStudent = function () {
+    return this._student;
+};

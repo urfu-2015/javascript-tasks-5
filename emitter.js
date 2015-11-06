aa8157dd167725166661f1c5a8d9f7e258b5af61
Anna1004 <anna.khlutkova@mail.ru>
2015-11-07 00:18:09 +0500

    +

--- emitter.js
+++ emitter.js
@@ -1,54 +1,63 @@
 module.exports = function () {
-
-    function getEvents(event) {
-        return event.split('.');
-    }
-
-    var students = [];
+    var subscriptions = [];
     return {
         on: function (eventName, student, callback) {
-            var newStudent = {
-                dataOfStudent: student,
+            subscriptions.push({
+                target: student,
                 event: eventName,
                 callBack: callback
-            };
-            students.push(newStudent);
+            });
         },
         off: function (eventName, student) {
-            for (var i = 0; i < students.length; i++) {
-                if (students[i] !== undefined &&
-                    students[i].event.indexOf(eventName) !== -1 &&
-                    students[i].dataOfStudent === student) {
-                    delete students[i];
+            for (var i = subscriptions.length; i >= 0; i--) {
+                if (subscriptions[i] !== undefined &&
+                    subscriptions[i].event.indexOf(eventName) !== -1 &&
+                    subscriptions[i].target === student) {
+                    subscriptions.splice(i, 1);
                 }
             }
         },
         emit: function (eventName) {
-            var eventFstLvl = getEvents(eventName)[0];
-            var eventSndLvl = getEvents(eventName)[1];
-            if (eventSndLvl === undefined) {
-                students.forEach(function (student) {
-                    if (student !== undefined &&
-                        student.event === eventFstLvl) {
-                        student.callBack.call(student.dataOfStudent);
+            var eventFstLvl = eventName.split('.')[0];
+            var eventSndLvl = eventName.split('.')[1];
+            subscriptions.forEach(function (student, index) {
+                if (student.typeOfEvent === 'several' && student.event === eventName) {
+                    if (student.currentCount < student.count) {
+                        student.callBack.call(student.target);
+                        student.currentCount++;
+                    } else {
+                        subscriptions.splice(index, 1);
                     }
-                });
-            } else {
-                students.forEach(function (student) {
-                    if (student !== undefined &&
-                            (student.event === eventName ||
-                            student.event.indexOf(eventFstLvl) !== -1)) {
-                        student.callBack.call(student.dataOfStudent);
+                };
+                if (student.typeOfEvent === 'through' && student.event === eventName) {
+                    if (student.currentStep % student.step === 0) {
+                        student.callBack.call(student.target);
                     }
-                });
-            }
+                    student.currentStep++;
+                };
+                if (student.typeOfEvent !== 'several' && student.typeOfEvent !== 'through') {
+                    if (eventSndLvl === undefined && student.event === eventFstLvl ||
+                    eventSndLvl !== undefined && student.event.indexOf(eventFstLvl) !== -1) {
+                        student.callBack.call(student.target);
+                    };
+                }
+            });
         },
         several: function (eventName, student, callback, n) {
-
+            subscriptions.push({target: student,
+                            event: eventName,
+                            callBack: callback,
+                            typeOfEvent: 'several',
+                            count: n,
+                            currentCount: 0});
         },
-
         through: function (eventName, student, callback, n) {
-
+            subscriptions.push({target: student,
+                            event: eventName,
+                            callBack: callback,
+                            typeOfEvent: 'through',
+                            step: n,
+                            currentStep: 1});
         }
     };
 };

cd763110e4e486dbaf40c63924a7c630ef60ab28
bazhenova <anyusha2595@gmail.com>
2015-11-08 00:25:56 +0500

    Исправления

--- emitter.js
+++ emitter.js
@@ -1,102 +1,88 @@
 module.exports = function () {
-    var students = [];
     var subscribedStudents = [];
     return {
         on: function (eventName, student, callback) {
-            if (students.indexOf(student) === -1) {
-                students.push(student);
-                var studentEvents = {}; //словарь событий
-                studentEvents[eventName] = {
-                    func: callback,
-                    period: 1,
-                    counter: 1, // счетчик, на каком шаге вызываем функцию
-                    max: Number.MAX_SAFE_INTEGER // максимальное число вызова функции
-                };
-                var newStudent = {
-                    data: student,
-                    events: studentEvents
-                };
-                subscribedStudents.push(newStudent);
+            var currentStudent = this.getStudent(student);
+            var _event = {
+                callback: callback,
+                period: 1,
+                counter: 1,
+                max: Number.MAX_SAFE_INTEGER
+            };
+            if (Object.keys(currentStudent).length === 0) {
+                var studentEvents = {};
+                studentEvents[eventName] = _event;
+                subscribedStudents.push(
+                    {
+                        ctx: student,
+                        events: studentEvents
+                    }
+                );
             } else {
-                var index = this.getIndex(student);
-                var keys = Object.keys(subscribedStudents[index]['events']);
-                if (keys.indexOf(eventName) !== -1) {
-                    subscribedStudents[index]['events'][eventName]['func'] = callback;
-                } else {
-                    subscribedStudents[index]['events'][eventName] = {
-                        func: callback,
-                        period: 1,
-                        counter: 1,
-                        max: Number.MAX_SAFE_INTEGER
-                    };
-                }
+                currentStudent.events[eventName] = _event;
             }
         },
 
-        getIndex: function (student) {
-            var index = 0;
+        getStudent: function (student) {
             for (var i = 0; i < subscribedStudents.length; i++) {
-                var currentStudent = subscribedStudents[i];
-                if (currentStudent.data === student) {
-                    return i;
+                if (subscribedStudents[i].ctx === student) {
+                    return subscribedStudents[i];
                 }
             }
-            return -1;
+            return {};
         },
 
         off: function (eventName, student) {
-            var index = this.getIndex(student);
-            if (index !== -1) {
-                var currentStudent = subscribedStudents[index];
-                var keys = Object.keys(currentStudent.events);
-                for (var i = 0; i < keys.length; i++) {
-                    if (keys[i].indexOf(eventName) !== -1) {
-                        delete currentStudent.events[keys[i]];
+            var currentStudent = this.getStudent(student);
+            if (Object.keys(currentStudent).length !== 0) {
+                var events = Object.keys(currentStudent.events);
+                for (var i = 0; i < events.length; i++) {
+                    if (events[i].indexOf(eventName) !== -1) {
+                        delete currentStudent.events[events[i]];
                     }
                 }
             }
         },
 
         emit: function (eventName) {
-            var namespace = this.getNamespace(eventName);
+            var namespaces = this.getNamespaces(eventName);
             for (var i = 0; i < subscribedStudents.length; i++) {
                 var student = subscribedStudents[i];
-                var keys = Object.keys(student.events);
-                for (var j = 0; j < keys.length; j++) {
-                    var currentEvent = student.events[keys[j]];
-                    if (keys[j] === eventName || keys[j] === namespace) {
-                        if (currentEvent.max > currentEvent.counter &&
-                            currentEvent.counter % currentEvent.period === 0) {
-                            currentEvent.func.call(student['data']);
+                var events = Object.keys(student.events);
+                for (var j = 0; j < events.length; j++) {
+                    var _event = student.events[events[j]];
+                    if (events[j] === eventName || namespaces.indexOf(events[j]) !== -1) {
+                        if (_event.max > _event.counter &&
+                            _event.counter % _event.period === 0) {
+                            _event.callback.call(student['ctx']);
                         }
-                        currentEvent.counter += 1;
+                        _event.counter += 1;
                     }
                 }
             }
         },
 
-        getNamespace: function (eventName) {
-            var index = eventName.indexOf('.');
-            var namespace = eventName;
-            if (index !== -1) {
-                namespace = eventName.slice(0, index);
+        getNamespaces: function (eventName) {
+            var namespaces = [];
+            var index = eventName.lastIndexOf('.');
+            while (index !== -1) {
+                var namespace = eventName.slice(0, index);
+                namespaces.push(namespace);
+                index = namespace.lastIndexOf('.');
             }
-            return namespace;
+            return namespaces;
         },
 
         several: function (eventName, student, callback, n) {
-            this.setAdditionalValue(eventName, student, callback, 'max', n);
+            this.on(eventName, student, callback);
+            var currentStudent = this.getStudent(student);
+            currentStudent.events[eventName]['max'] = n;
         },
 
         through: function (eventName, student, callback, n) {
-            this.setAdditionalValue(eventName, student, callback, 'period', n);
-        },
-
-        setAdditionalValue: function (eventName, student, callback, nameValue, n) {
             this.on(eventName, student, callback);
-            var index = this.getIndex(student);
-            var currentStudent = subscribedStudents[index];
-            currentStudent.events[eventName][nameValue] = n;
+            var currentStudent = this.getStudent(student);
+            currentStudent.events[eventName]['period'] = n;
         }
     };
 };

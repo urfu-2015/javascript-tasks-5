742694daedd5b86218d4755eedd85047f92b496e
davletova <davletova.a.g@gmail.com>
2015-11-09 00:44:28 +0500

    Update emitter.js

--- emitter.js
+++ emitter.js
@@ -1,54 +1,58 @@
 module.exports = function () {
-	var events_subscribers = {};
+	var eventsSubscribers = {};
 	var students = [];
     return {
         on: function (eventName, student, callback) {
-			var i = students.indexOf(student);
-			if (i < 0) {
-				i = students.length;
-				students[i] = student;
-			}
-			if (!events_subscribers.hasOwnProperty(eventName)) {
-				events_subscribers[eventName] = {};
-			}
-			events_subscribers[eventName][i] = callback;
+        var numSt = students.indexOf(student);
+        if (numSt < 0) {
+                numSt = students.length;
+                students[numSt] = student;
+            }
+            if (!eventsSubscribers[eventName]) {
+                eventsSubscribers[eventName] = {};
+            }
+            if (!eventsSubscribers[eventName][numSt]) {
+                eventsSubscribers[eventName][numSt] = [];
+            }
+            eventsSubscribers[eventName][numSt].push(callback);
         },
 
         off: function (eventName, student) {
-			var delSubscriber = function(i, evName) {
-				if (events_subscribers.hasOwnProperty(evName) && events_subscribers[evName].hasOwnProperty(i)) {
-					delete events_subscribers[evName][i];
-				}
-			}
-			var i = students.indexOf(student);
-			if (i >= 0) {
-				for(var event in events_subscribers) {
-					if (event.indexOf(eventName) >= 0) {
-						delSubscriber(i,event);
-					}
-				}
-			}
-			return events_subscribers;
+            var delSubscriber = function (numSt, evName) {
+                if (eventsSubscribers[evName] && eventsSubscribers[evName][numSt]) {
+                    delete eventsSubscribers[evName][numSt];
+                }
+            }
+            var numSt = students.indexOf(student);
+            if (numSt >= 0) {
+                for(var event in eventsSubscribers) {
+                    if ((event === eventName) || (event.indexOf(eventName+'.') >= 0)) {
+                        delSubscriber(numSt,event);
+                    }
+                }
+            }
         },
 
         emit: function (eventName) {
-			var perform = function(evName) {
-				if (events_subscribers.hasOwnProperty(evName)) {
-					var subscribers = events_subscribers[evName];
-					for(var numSt in subscribers) {
-						subscribers[numSt].call(students[numSt]);
-					}
-				}
-			}
-			perform(eventName);
-			var subEventName = eventName.split('.')[0];
-			if (!(subEventName === eventName)) {
-				perform(subEventName);
-			}
+            var perform = function (evName) {
+                if (eventsSubscribers[evName]) {
+                    var subscribers = eventsSubscribers[evName];
+                    for(var numSt in subscribers) {
+                        for (var j = 0; j < subscribers[numSt].length; j++) {
+                            subscribers[numSt][j].call(students[numSt]);
+                        }
+                    }
+                }
+            }
+            perform(eventName);
+            var subEventName = eventName.split('.')[0];
+            if (subEventName !== eventName) {
+                perform(subEventName);
+            }
         },
 
         several: function (eventName, student, callback, n) {
-
+			
         },
 
         through: function (eventName, student, callback, n) {

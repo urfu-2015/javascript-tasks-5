0dbfd50c072a786838da74b72d84a05fc3c8670f
ValeryZhurba <valera.zhurba@gmail.com>
2015-11-09 00:35:36 +0500

    Исправил баги

--- emitter.js
+++ emitter.js
@@ -2,10 +2,18 @@ module.exports = function () {
     return {
         events: {},
 
+        checkEvent: function (callCount, callPeriodicity) {
+            if ((callCount < 0) || (callPeriodicity < 0)) {
+                throw new Error('значения периодичности вызова обработчика и количества вызовов +' +
+                    ' обработчика должны быть неотрицательными');
+            }
+        },
+
         on: function (eventName, student, callback, callCount, callPeriodicity) {
             if (!(eventName in this.events)) {
                 this.events[eventName] = [];
             }
+            this.checkEvent(callCount, callPeriodicity);
             if ((callCount === undefined) || (callCount === null)) {
                 callCount = Number.POSITIVE_INFINITY;
             }
@@ -22,7 +30,7 @@ module.exports = function () {
 
         off: function (eventName, student) {
             Object.keys(this.events).forEach(function (key) {
-                if (key.indexOf(eventName) >= 0) {
+                if (key.indexOf(eventName) > -1) {
                     for (var i = 0; i < this.events[key].length; i++) {
                         if (this.events[key][i].student === student) {
                             this.events[key].splice(i, 1);
@@ -33,37 +41,40 @@ module.exports = function () {
             }, this);
         },
 
-        emit: function (eventName) {
-            function createEventList() {
-                var currentEvents = eventName.split('.');
-                if (currentEvents.length === 1) {
-                    return [eventName];
-                } else {
-                    var curEvent = currentEvents[0];
-                    var eventList = [curEvent];
-                    for (var i = 1; i < currentEvents.length; i++) {
-                        curEvent = curEvent + '.' + currentEvents[i];
-                        eventList.push(curEvent);
-                    }
-                    return eventList;
+        createEventList: function (eventName) {
+            var currentEvents = eventName.split('.');
+            if (currentEvents.length === 1) {
+                return [eventName];
+            } else {
+                var curEvent = currentEvents[0];
+                var eventList = [curEvent];
+                for (var i = 1; i < currentEvents.length; i++) {
+                    curEvent = curEvent + '.' + currentEvents[i];
+                    eventList.push(curEvent);
                 }
+                return eventList;
             }
+        },
+
+        emit: function (eventName) {
             if (eventName in this.events) {
-                var eventList = createEventList();
+                var eventList = this.createEventList(eventName);
                 eventList.forEach(function (eventName) {
-                    this.events[eventName].forEach(function (currentEvent) {
-                        var student = currentEvent.student;
-                        currentEvent.callNunber += 1;
-                        if (currentEvent.callCount < 1) {
-                            this.off(eventName, student);
-                            return;
-                        }
-                        if (currentEvent.callNunber % currentEvent.callPeriodicity === 0) {
-                            currentEvent.callCount -= 1;
-                            var callback = currentEvent.callback;
-                            callback.apply(student);
-                        }
-                    });
+                    if (Object.keys(this.events).indexOf(eventName) > -1) {
+                        this.events[eventName].forEach(function (currentEvent) {
+                            var student = currentEvent.student;
+                            currentEvent.callNunber += 1;
+                            if (currentEvent.callCount < 1) {
+                                this.off(eventName, student);
+                                return;
+                            }
+                            if (currentEvent.callNunber % currentEvent.callPeriodicity === 0) {
+                                currentEvent.callCount -= 1;
+                                var callback = currentEvent.callback;
+                                callback.apply(student);
+                            }
+                        }, this);
+                    }
                 }, this);
             }
         },

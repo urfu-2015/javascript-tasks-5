53abb298460da55abe5572f1196dc5221f7548a3
Даниил <Tkach@MacBook-Air-Daniil.local>
2015-11-08 14:18:40 +0500

    fixed codeStyle

--- emitter.js
+++ emitter.js
@@ -20,7 +20,6 @@ module.exports = function () {
                         }]
                     });
         },
-
         off: function (eventName, student) {
             for (var i = 0; i < students.length; i++) {
                 if (students[i].studentName === student) {
@@ -29,33 +28,31 @@ module.exports = function () {
                     });
                 }
             }
-         },
-
+        },
         emit: function (eventName) {
             students.forEach(function (student) {
                 var listEvent = eventName.split('.');
-                var oneEvent = listEvent[0]
+                var oneEvent = listEvent[0];
                 for (var i = 0; i < listEvent.length; i++) {
                     if (i > 0) {
-                    oneEvent += '.' + listEvent[i];
+                        oneEvent += '.' + listEvent[i];
                     }
                     student.events.forEach(function (event) {
-                    if (event.eventName === oneEvent) {
-                        var callBack = event.func;
-                        callBack.apply(student.studentName);
+                        if (event.eventName === oneEvent) {
+                            var callBack = event.func;
+                            callBack.apply(student.studentName);
                         }
                     });
                 }
             });
         }
-    }
-}
-
-function getIndexStudentName (array, key) {
+    };
+};
+function getIndexStudentName(array, key) {
     for (var i = 0; i < array.length; i++) {
         if (array[i].studentName === key) {
             return i;
         }
     }
-        return -1;
+    return -1;
 }

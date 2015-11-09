955b7215a469c061d6e570a5b0ee7091380c051f
Rybalka Ekaterina <lonelygopnik@gmail.com>
2015-11-09 21:50:01 +0500

    Update emitter.js

--- emitter.js
+++ emitter.js
@@ -10,30 +10,30 @@ module.exports = function () {
         },
 
         off: function (eventName, student) {
-            if (this.students.indexOf(student) > -1) {
-                delete student.events[eventName];
-                var steps = eventName.split('.');
-                for (var event in student.events) {
-                    if (student.events.hasOwnProperty(event) && steps.length == 1 &&
-                        event.indexOf(steps[0]) > -1) {
-                        delete student.events[event];
-                    }
+            if (this.students.indexOf(student) < 0) {
+                return;
+            }
+            delete student.events[eventName];
+            var steps = eventName.split('.');
+            for (var event in student.events) {
+                if (student.events.hasOwnProperty(event) && steps.length == 1 &&
+                    event.indexOf(steps[0]) > -1) {
+                    delete student.events[event];
                 }
             }
         },
 
         emit: function (eventName) {
             var listEvents = eventName.split('.');
-            var f = (student) => {
+            this.students.forEach(student => {
                 if (this.students.indexOf(student) > -1 &&
                     student.events.hasOwnProperty(listEvents[0])) {
-                    student.events[listEvents[0]].apply(student);
-                    if (listEvents.length > 1 && student.events.hasOwnProperty(eventName)) {
-                        student.events[eventName].apply(student);
-                    }
+                student.events[listEvents[0]].apply(student);
                 }
-            };
-            this.students.forEach(f);
+                if (listEvents.length > 1 && student.events.hasOwnProperty(eventName)) {
+                    student.events[eventName].apply(student);
+                }
+            });
         },
 
         several: function (eventName, student, callback, n) {

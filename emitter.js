1ccfb4af56a3d754ada9fe271e224499497db3da
KladovValery <valerijkladov@yandex.ru>
2015-11-10 01:53:22 +0500

    Update emitter.js

--- emitter.js
+++ emitter.js
@@ -1,34 +1,64 @@
 module.exports = function () {
-    var allEvents = [];
+    var subscriptions = [];
     return {
         on: function (eventName, student, callback) {
-            var eventIndex = getIndexEventName(allEvents, 'eventName', eventName);
- 
-            return eventIndex > 0 ?
-                allEvents[eventIndex].participants.push(student)
-                :
-                allEvents.push(
-                    {
-                        eventName: eventName, 
-                        participants: [student],
-                        senceEvent: callback
-                    });
-        },
-
-        off: function (eventName, student) {
-            var indexEvent = getIndexEventName(allEvents, 'eventName', eventName);
-            return allEvents[indexEvent].participants.filter(function (name) {
-                return name !== student;
+            subscriptions.push({
+                target: student,
+                event: eventName,
+                callBack: callback
             });
         },
-
-    function getIndexEventName (array, property, key) {
-    for (var i = 0; i < array.length; i++) {
-        if (array[i].property === key) {
-            return i;
+        off: function (eventName, student) {
+            for (var i = subscriptions.length; i >= 0; i--) {
+                if (subscriptions[i] !== undefined &&
+                    subscriptions[i].event.indexOf(eventName) !== -1 &&
+                    subscriptions[i].target === student) {
+                    subscriptions.splice(i, 1);
+                }
+            }
+        },
+        emit: function (eventName) {
+            var eventFstLvl = eventName.split('.')[0];
+            var eventSndLvl = eventName.split('.')[1];
+            subscriptions.forEach(function (student, index) {
+                if (student.typeOfEvent === 'several' && student.event === eventName) {
+                    if (student.currentCount < student.count) {
+                        student.callBack.call(student.target);
+                        student.currentCount++;
+                    } else {
+                        subscriptions.splice(index, 1);
+                    }
+                };
+                if (student.typeOfEvent === 'through' && student.event === eventName) {
+                    if (student.currentStep % student.step === 0) {
+                        student.callBack.call(student.target);
+                    }
+                    student.currentStep++;
+                };
+                if (student.typeOfEvent !== 'several' && student.typeOfEvent !== 'through') {
+                    if (eventSndLvl === undefined && student.event === eventFstLvl ||
+                    eventSndLvl !== undefined && student.event.indexOf(eventFstLvl) !== -1) {
+                        student.callBack.call(student.target);
+                    };
+                }
+            });
+        },
+        several: function (eventName, student, callback, n) {
+            subscriptions.push({target: student,
+                            event: eventName,
+                            callBack: callback,
+                            typeOfEvent: 'several',
+                            count: n,
+                            currentCount: 0});
+        },
+        through: function (eventName, student, callback, n) {
+            subscriptions.push({target: student,
+                            event: eventName,
+                            callBack: callback,
+                            typeOfEvent: 'through',
+                            step: n,
+                            currentStep: 1});
         }
-    }
-        return -1;
-}
-
+    };
 };
+

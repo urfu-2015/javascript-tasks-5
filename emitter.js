bcddbfd925ae17d8acdecd6003745b5fb83b3b4c
eimic <korabelnikovat@gmail.com>
2015-11-13 02:10:25 +0500

    change

--- emitter.js
+++ emitter.js
@@ -1,18 +1,21 @@
 module.exports = function () {
-    var events = [];
+    var events = {};
     return {
         on: function (eventName, person, callback) {
             var event = {
                 person: person,
                 callback: callback
             };
-            if (events[eventName] === undefined) {
+            if (!events.hasOwnProperty(eventName)) {
                 events[eventName] = [];
             }
             events[eventName].push(event);
         },
 
         off: function (eventName, person) {
+            if (eventName === '') {
+                return false;
+            }
             Object.keys(events).forEach(function (key) {
                 if (key.indexOf(eventName) !== -1) {
                     var changeEvents = events[key].filter(function (event) {
@@ -24,27 +27,67 @@ module.exports = function () {
         },
 
         emit: function (eventName) {
-            var namespace = [eventName];
-            while (true) {
-                var dote = eventName.lastIndexOf('.');
-                if (dote !== -1) {
-                    eventName = eventName.slice(0, dote);
-                    namespace.push(eventName);
-                } else {
-                    break;
-                }
-            }
+            var namespace = getNamespace(eventName);
             namespace.forEach(function (name) {
-                if (events[name] !== undefined) {
+                if (events.hasOwnProperty(name)) {
                     events[name].forEach(function (event) {
-                        event.callback.call(event.person);
+                        if (event.hasOwnProperty('several') && event.several > 0) {
+                            event.callback.call(event.person);
+                            --event.several;
+                        }
+                        if (event.hasOwnProperty('through') &&
+                            event.current % event.through === 0) {
+                            event.callback.call(event.person);
+                            ++event.current;
+                        } else {
+                            ++event.current;
+                        }
+                        if (!event.hasOwnProperty('several') &&
+                            !event.hasOwnProperty('through')) {
+                            event.callback.call(event.person);
+                        }
                     });
                 }
             });
         },
 
-        several: function (eventName, student, callback, n) {},
+        several: function (eventName, person, callback, n) {
+            var event = {
+                person: person,
+                callback: callback,
+                several: n
+            };
+            if (!events.hasOwnProperty(eventName)) {
+                events[eventName] = [];
+            }
+            events[eventName].push(event);
+        },
 
-        through: function (eventName, student, callback, n) {}
+        through: function (eventName, person, callback, n) {
+            var event = {
+                person: person,
+                callback: callback,
+                through: n,
+                current: 1
+            };
+            if (!events.hasOwnProperty(eventName)) {
+                events[eventName] = [];
+            }
+            events[eventName].push(event);
+        }
     };
 };
+
+function getNamespace(name) {
+    var namespace = [name];
+    while (true) {
+        var dote = name.lastIndexOf('.');
+        if (dote !== -1) {
+            name = name.slice(0, dote);
+            namespace.push(name);
+        } else {
+            break;
+        }
+    }
+    return namespace;
+}

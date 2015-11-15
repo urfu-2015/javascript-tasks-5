8b0c60787aecc82ab248673c6088e12438024220
Rybalka Ekaterina <lonelygopnik@gmail.com>
2015-11-15 20:15:16 +0500

    Update emitter.js

--- emitter.js
+++ emitter.js
@@ -2,38 +2,110 @@ module.exports = function () {
     return {
         students: [],
         on: function (eventName, student, callback) {
-            if (this.students.indexOf(student) < 0) {
-                student.events = {};
-                this.students.push(student);
+            var k = 0;
+            var arg = eventName.split(".");
+            var next;
+            var ok = false;
+            var i = 0;
+            next = findStudent(this.students, student);
+            if (next === undefined) {
+                this.students.push({
+                    character: student,
+                    events: [{
+                        eventName: arg[k]
+                    }]
+                });
+                next = this.students[this.students.length - 1].events;
             }
-            student.events[eventName] = callback;
+            while (k < arg.length - 1) {
+                for (i = 0; i < next.length; i++) {
+                    if (next[i].eventName === arg[k]) {
+                        if (!next[i].hasOwnProperty("nextEv")) {
+                            next[i].nextEv = [{
+                                eventName: arg[k + 1]
+                            }];
+                        }
+                        next = next[i].nextEv;
+                        ok = true;
+                        break;
+                    }
+                }
+                if (!ok) {
+                    next.push({
+                        eventName: arg[k],
+                        nextEv: [{
+                            eventName: arg[k + 1]
+                        }]
+                    });
+                    next = next[i].nextEv;
+                }
+                k += 1;
+            }
+            ok = false;
+            for (i = 0; i < next.length; i ++) {
+                if (next[i].eventName === arg[k] && !next[i].hasOwnProperty("callback")) {
+                    next[i].callback = callback;
+                    ok = true;
+                    break;
+                }
+            }
+            if (!ok) {
+                next.push({
+                    eventName: arg[k],
+                    callback: callback
+                })
+            }
         },
 
         off: function (eventName, student) {
-            if (this.students.indexOf(student) < 0) {
-                return;
-            }
-            delete student.events[eventName];
-            var steps = eventName.split('.');
-            for (var event in student.events) {
-                if (student.events.hasOwnProperty(event) && steps.length == 1 &&
-                    event.indexOf(steps[0]) > -1) {
-                    delete student.events[event];
+            var k = 0;
+            var arg = eventName.split(".");
+            var next = findStudent(this.students, student);
+            var c_next;
+            if (next != undefined) {
+                while (next.length != 0 && k < arg.length) {
+                    c_next = [];
+                    next.forEach(function (ev) {
+                        if (ev.eventName === arg[k]) {
+                            if (k === arg.length - 1) {
+                                delete ev.callback;
+                                delete ev.nextEv;
+                            } else if (ev.hasOwnProperty("nextEv")) {
+                                c_next = c_next.concat(ev.nextEv);
+                            }
+                        }
+                    });
+                    next = c_next;
+                    k += 1;
                 }
             }
         },
 
         emit: function (eventName) {
-            var listEvents = eventName.split('.');
-            this.students.forEach(student => {
-                if (this.students.indexOf(student) > -1 &&
-                    student.events.hasOwnProperty(listEvents[0])) {
-                    student.events[listEvents[0]].apply(student);
+            var k;
+            var arg = eventName.split(".");
+            var next;
+            var c_next;
+            this.students.forEach(function (st) {
+                k = 0;
+                next = st.events;
+                while (next.length != 0 && k < arg.length) {
+                    c_next = [];
+                    next.forEach(function (ev) {
+                        if (ev.eventName === arg[k]) {
+                            //console.log(ev);
+                            if (ev.hasOwnProperty("callback")) {
+                                ev.callback.apply(st.character);
+                            }
+                            if (ev.hasOwnProperty("nextEv")) {
+                                c_next = c_next.concat(ev.nextEv);
+                            }
+                        }
+                    });
+                    next = c_next;
+                    k += 1;
                 }
-                if (listEvents.length > 1 && student.events.hasOwnProperty(eventName)) {
-                    student.events[eventName].apply(student);
-                }
-            });
+            })
         },
 
         several: function (eventName, student, callback, n) {
@@ -45,3 +117,11 @@ module.exports = function () {
         }
     };
 };
+
+function findStudent(students, student) {
+    for (var i = 0; i < students.length; i++) {
+        if (students[i].character === student) {
+            return students[i].events;
+        }
+    }
+}

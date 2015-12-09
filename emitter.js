2d4bc3383f5bd680bc0550d25b8512c00d563a53
Денис Акимов <554dgdf@gmail.com>
2015-12-09 17:14:32 +0500

    Акимов Денис
    Без допа

--- emitter.js
+++ emitter.js
@@ -1,23 +1,37 @@
 module.exports = function () {
-    return {
+    var students = [];
+	var index = 0;
+	var currentEvent;
+	return {
+	    index, 
+	    students: [],
         on: function (eventName, student, callback) {
-
+            var studentActive = {name: student, event: eventName, callback: callback};
+			this.students.push(studentActive);
         },
 
         off: function (eventName, student) {
-
-        },
+            for (var i = 0; i < this.students.length; i++) {
+			    if ((this.students[i].event === eventName) && (this.students[i].name === student)) {
+				    this.students.splice(i,1);
+				}
+			}
+		},
 
         emit: function (eventName) {
+            this.students.forEach(function(student) {
+			    if (eventName.indexOf(student.event) != -1) {
+				    student.callback.call(student);
+				}
+			});			
+        }
 
-        },
-
-        several: function (eventName, student, callback, n) {
+        /*several: function (eventName, student, callback, n) {
 
         },
 
         through: function (eventName, student, callback, n) {
 
-        }
+        }*/
     };
 };

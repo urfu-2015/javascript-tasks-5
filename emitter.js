cb8499754c03754dcc3f9370e640df450c601e12
Денис Акимов <554dgdf@gmail.com>
2015-12-14 21:57:23 +0500

    update
    поправил off, теперь отписывает более правильно
    Поправил emit

--- emitter.js
+++ emitter.js
@@ -2,29 +2,52 @@ module.exports = function () {
     var students = [];
 	var index = 0;
 	var currentEvent;
-	return {
-	    index, 
-	    students: [],
+	
+    var willDelete = function (Target, Array) {
+	    var answer = true;
+        if (Array.length < Target.length) {
+            answer = false;
+        } else {
+            for (var i = 0; i < Target.length; i++) {
+                if (Target[i] != Array[i]) {
+                    answer = false;
+                    break;
+                }
+            }
+        }
+        return answer;
+    };
+    return {
+        index, 
+        students: [],
         on: function (eventName, student, callback) {
             var studentActive = {name: student, event: eventName, callback: callback};
-			this.students.push(studentActive);
+            this.students.push(studentActive);
         },
 
         off: function (eventName, student) {
+            var eventList = eventName.split('.');
             for (var i = 0; i < this.students.length; i++) {
-			    if ((this.students[i].event).indexOf(eventName) != -1 && (this.students[i].name === student)) {
-				    this.students.splice(i,1);
-					i--;
-				}
-			}
-		},
+                var studenEventList = (this.students[i].event).split('.'); 
+                if (willDelete(eventList, studenEventList) && (this.students[i].name === student)) {
+                    this.students.splice(i,1);
+                    i--;
+                }
+            }
+        },
 
         emit: function (eventName) {
-            this.students.forEach(function(student) {
-			    if (eventName.indexOf(student.event) != -1) {
-				    student.callback.call(student);
-				}
-			});			
+            var eventList = eventName.split('.');	
+            for (var i = 1; i < eventList.length; i++) {
+                eventList[i] = eventList[i - 1] + '.' + eventList[i];
+            }
+            for (var i = 0; i < eventList.length; i++) {
+                for (var j = 0; j < this.students.length; j++) {
+                    if (eventList[i] === this.students[j].event) {
+                        this.students[j].callback.call(this.students[j].name);
+                    } 
+                }
+            }
         }
 
         /*several: function (eventName, student, callback, n) {
